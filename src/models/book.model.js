97a94edbefb33d75a63fa2cac55c38e51ab391fe
Harish1-1 <109864814+Harish1-1@users.noreply.github.com>
2024-08-26 20:45:05 +0530

    Update book.model.js

--- src/models/book.model.js
+++ src/models/book.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const bookSchema = new mongoose.Schema({
-  bookId: { type: String, default: uuidv4, unique: true },  // Adding bookId with UUID
+  bookId: { type: String, default: uuidv4, unique: true },  
   title: { type: String, required: true },
   description: String,
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author', required: true },
