d71efb84783aa72653a6fa8a6f09175760589251
Harish1-1 <109864814+Harish1-1@users.noreply.github.com>
2024-08-26 20:44:50 +0530

    Update author.model.js

--- src/models/author.model.js
+++ src/models/author.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const authorSchema = new mongoose.Schema({
-  authorId: { type: String, default: uuidv4, unique: true },  // Adding authorId with UUID
+  authorId: { type: String, default: uuidv4, unique: true },  
   name: { type: String, required: true, unique: true },
   bio: String,
 });
